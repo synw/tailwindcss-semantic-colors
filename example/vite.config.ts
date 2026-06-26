8c85d572df20420e7ea9e4d2ad3c6c898434f999
synw <synwe@yahoo.com>
2026-06-26 15:50:28 +0200

    build example site

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
@@ -6,4 +7,15 @@ export default defineConfig({
     vue(),
     tailwindcss(),
   ],
+  assetsInclude: ["**/*.md"],
+  base: process.env.NODE_ENV === 'production' ? "/tailwindcss-semantic-colors" : './',
+  resolve: {
+    alias: [
+      { find: '@/', replacement: '/src/' },
+      {
+        find: 'vue',
+        replacement: path.resolve("./node_modules/vue"),
+      },
+    ]
+  },
 })
